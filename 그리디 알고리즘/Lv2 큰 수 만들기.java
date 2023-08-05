731b8c0f7f97c72fc6fd7a4adb89ff54aff8a32c
rhdiddl9537 <whanw7824@naver.com>
2023-08-05 17:34:13 +0900

    큰 수 만들기

--- 그리디 알고리즘/Lv2 큰 수 만들기.java
+++ 그리디 알고리즘/Lv2 큰 수 만들기.java
@@ -4,9 +4,7 @@
 
     수를 제거하되 숫자 나열 순서를 지켜야함
     안쪽 for문에서 조정
-    (첫번재로 올 수 있는 수는 1번째부터 number.length()-k번째의 수까지. 그 중에서 최대값을 구한다)
-
-
+    (첫번재로 올 수 있는 수는 첫번째부터 k 번째의 수까지. 그 중에서 최대값을 구한다)
  */
 
 
@@ -35,3 +33,36 @@ class Solution {
         return answer;
     }
 }
+
+
+
+
+
+/*
+    스택사용방식
+    스택을 사용하여 제거할 수 k개를 pop하는 방식
+ */
+
+import java.util.Stack;
+class Solution {
+    public String solution(String number, int k) {
+        char[] answer = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && (stack.peek() < c) && k > 0) {
+                stack.pop();
+                k--;
+            }
+            stack.push(c);
+
+        }
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = stack.get(i);
+        }
+        return new String(answer);
+    }
+}
+
